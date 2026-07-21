2e405dc553ad006d1dda934ac1fb7de84c5a45fc
theryanms <60490595+theryanms@users.noreply.github.com>
2026-07-20 22:56:07 -0700

    Added speed insights import to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
